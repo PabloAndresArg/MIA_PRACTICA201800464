2a5417b48dda70d7063082bfae720397420d8adb
Pablo Andres Argueta <pabloandresarg@gmail.com>
2020-09-27 18:39:26 -0600

    nodemon
    ok

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const http = require('http');// para hacer peticiones http 
-http.createServer(function (req , res){// funcion de parametro 
-    res.write('<h1>hola mundo nodejs</h1>');
-    res.end(); 
-}).listen(3000);
-// node usa el sistema operativo
+const exp = require('express')
+const app = exp();// app es el servidor  
+app.listen(3000 , () => {console.log("SERVIDOR INICIADO EN localhost:3000")});
+app.get('/', (solicitud ,respuesta)=>{respuesta.send('hello world')});
+app.get('/root', (solicitud ,respuesta)=>{respuesta.send('root')});
+app.post('/', (solicitud ,respuesta)=>{respuesta.send('enviaron algo')});

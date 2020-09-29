b7a8c62f2295ca66fe69b1480c5f4a3dcc682f91
Pablo Andres Argueta <pabloandresarg@gmail.com>
2020-09-28 23:46:27 -0600

    typescript ok
    oko

--- main.js
+++
@@ -1,6 +0,0 @@
-const exp = require('express')
-const app = exp();// app es el servidor  
-app.listen(3000 , () => {console.log("SERVIDOR INICIADO EN localhost:3000")});
-app.get('/', (solicitud ,respuesta)=>{respuesta.send('hello world')});
-app.get('/root', (solicitud ,respuesta)=>{respuesta.send('root')});
-app.post('/', (solicitud ,respuesta)=>{respuesta.send('enviaron algo')});

bf8aa5beeb75ef22871cbfb78f8370864a269020
Pablo Andres Argueta Hernandez <pabloandresarg@gmail.com>
2020-10-02 21:34:34 -0600

    proveedores  y clientes cargados al ER
    ok

--- src/Consultas/[MIA]CargaDeDatos.sql
+++ src/Consultas/[MIA]CargaDeDatos.sql
@@ -5,8 +5,8 @@ character set latin1
 FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 lines
-(nombreComp,contactoComp,correoComp,telefonoComp,tipo,nombre,correo,tel,fechaRegistro,direccion,ciudad,codigoPostal,region,producto,categoriaProducto,cantidad,precioUnitario)
-SET fechaRegistro=str_to_date(@var1,'%d%m%Y');
+(nombreComp,contactoComp,correoComp,telefonoComp,tipo,nombre,correo,tel,@var1,direccion,ciudad,codigoPostal,region,producto,categoriaProducto,cantidad,precioUnitario)
+SET fechaRegistro = str_to_date( @var1 ,'%d/%m/%Y');
 
 
 /* CARGANDO LA TABLA ER */
@@ -23,6 +23,35 @@ where categoria.categoria = temporal.categoriaProducto;
 INSERT INTO compania (nombre , contacto , correo , telefono ) select distinct temporal.nombreComp , temporal.contactoComp , temporal.correoComp , temporal.telefonoComp
 FROM temporal;
 /*POSTAL*/
-INSERT INTO codigoPostal (codigoPostal) select distinct codigoPostal from temporal; 
+INSERT INTO codigoPostal (codigoPostal) 
+select distinct temporal.codigoPostal from temporal; 
 /*CIUDAD*/
 INSERT INTO ciudad (nombreCiudad) select distinct temporal.ciudad FROM temporal; 
+/*INSERTANDO EN DIRECCIONES*/
+INSERT INTO direccion (direccion , idCodigoPostal  , idCiudad , idRegion)
+SELECT  distinct
+temporal.direccion , codigoPostal.idCodigoPostal , ciudad.idCiudad , region.idRegion
+FROM temporal, codigoPostal,ciudad, region
+WHERE temporal.codigoPostal = codigoPostal.codigoPostal 
+AND temporal.ciudad = ciudad.nombreCiudad
+AND temporal.region = region.region; 
+/*CLIENTES*/
+INsert into cliente 
+(nombre , correo , telefono , fechaRegistro , idDireccion)
+SELECT distinct
+temporal.nombre , temporal.correo
+,temporal.tel , temporal.fechaRegistro
+,direccion.idDireccion
+FROM direccion , temporal
+WHERE temporal.tipo = 'C' 
+AND direccion.direccion = temporal.direccion;
+/*PROVEEDORES*/
+INsert into proveedor 
+(nombre , correo , telefono , fechaRegistro , idDireccion)
+SELECT distinct
+temporal.nombre , temporal.correo
+,temporal.tel , temporal.fechaRegistro
+,direccion.idDireccion
+FROM direccion , temporal
+WHERE temporal.tipo = 'P' 
+AND direccion.direccion = temporal.direccion;
--- src/Consultas/[MIA]InstruccionesDDL.sql
+++ src/Consultas/[MIA]InstruccionesDDL.sql
@@ -1,186 +1,174 @@
 /*CREACION DE LA TABLA TEMPORAL*/
-CREATE TABLE temporal (
-	nombreComp varchar(60) not null,
-    contactoComp varchar(60) not null,
-    correoComp varchar(60) not null,
-    telefonoComp varchar(15) not null,
-    tipo char(1) not null,
-    nombre varchar(60) not null,
-    correo varchar(60) not null,
-    tel	   varchar(15) not null,
-    fechaRegistro date not null, 
-    direccion varchar (60) not null, 
-    ciudad varchar(60) not null,
-    codigoPostal int not null,
-    region varchar(60) not null,
-    producto varchar(60) not null,
-    categoriaProducto varchar(60) not null,
-    cantidad int not null,
-    precioUnitario decimal(4) not null
-);
+    CREATE TABLE temporal (
+        nombreComp varchar(60) not null,
+        contactoComp varchar(60) not null,
+        correoComp varchar(60) not null,
+        telefonoComp varchar(15) not null,
+        tipo char(1) not null,
+        nombre varchar(60) not null,
+        correo varchar(60) not null,
+        tel	   varchar(15) not null,
+        fechaRegistro date not null, 
+        direccion varchar (60) not null, 
+        ciudad varchar(60) not null,
+        codigoPostal int not null,
+        region varchar(60) not null,
+        producto varchar(60) not null,
+        categoriaProducto varchar(60) not null,
+        cantidad int not null,
+        precioUnitario decimal(4) not null
+    );
 /* CREACION DE MODELO RELACIONAL */
-/* QUERYES PARA MI BASE DE DATOS */
-use p1;
-/*				NO DEPENDIENTES 			*/
-CREATE TABLE IF NOT EXISTS compania(
-	idCompania INT not null auto_increment, 
-    nombre varchar(60) not null, 
-    contacto varchar(60) not null,
-    correo varchar(60) not null ,
-    telefono varchar(60) not null, 
-	PRIMARY KEY (idCompania)
-);
+    use p1;
+    /*				NO DEPENDIENTES 			*/
+    CREATE TABLE IF NOT EXISTS compania(
+        idCompania INT not null auto_increment, 
+        nombre varchar(60) not null, 
+        contacto varchar(60) not null,
+        correo varchar(60) not null ,
+        telefono varchar(60) not null, 
+        PRIMARY KEY (idCompania)
+    );
 
-CREATE TABLE IF NOT exists ciudad(
-	idCiudad INT NOT NULL auto_increment,
-    nombreCiudad varchar(60) not null, 
-    PRIMARY KEY (idCiudad)
-);
+    CREATE TABLE IF NOT exists ciudad(
+        idCiudad INT NOT NULL auto_increment,
+        nombreCiudad varchar(60) not null, 
+        PRIMARY KEY (idCiudad)
+    );
 
-CREATE TABLE IF NOT exists region (
-	idRegion INT NOT NULL auto_increment,
-    region varchar(60) not NULL, 
-    PRIMARY KEY (idRegion)
-);
-CREATE TABLE IF NOT EXISTS codigoPostal(
-	idCodigoPostal INT NOT NULL AUTO_INCREMENT,
-    codigoPostal INT  not null,
-    PRIMARY KEY (idCodigoPostal)
-);
+    CREATE TABLE IF NOT exists region (
+        idRegion INT NOT NULL auto_increment,
+        region varchar(60) not NULL, 
+        PRIMARY KEY (idRegion)
+    );
+    CREATE TABLE IF NOT EXISTS codigoPostal(
+        idCodigoPostal INT NOT NULL AUTO_INCREMENT,
+        codigoPostal INT  not null,
+        PRIMARY KEY (idCodigoPostal)
+    );
 
-/*		TABLAS DEPENDIENTES O CON LLAVES FORANEAS	*/
-CREATE TABLE IF NOT EXISTS direccion (
-	idDireccion INT NOT NULL auto_increment, 
-    direccion varchar(60) not null,
-    idCodigoPostal Int not null,
-    PRIMARY KEY (idDireccion),
-	foreign key (idCodigoPostal) REFERENCES codigoPostal (idCodigoPostal) 
-); 	
-create table IF NOT EXISTS categoria (
-	idCategoria int not null auto_increment, 
-    categoria varchar(60) not  null,
-    PRIMARY KEY (idCategoria)
-);
+    /*		TABLAS DEPENDIENTES O CON LLAVES FORANEAS	*/
+    CREATE TABLE IF NOT EXISTS direccion (
+        idDireccion INT NOT NULL auto_increment, 
+        direccion varchar(60) not null,
+        idCodigoPostal Int not null,
+        idCiudad int not null, 
+        idRegion int not null,
+        PRIMARY KEY (idDireccion),
+        foreign key (idCodigoPostal) REFERENCES codigoPostal (idCodigoPostal) ,
+        foreign key (idCiudad) REFERENCES ciudad (idCiudad) ,
+        foreign key (idRegion) REFERENCES region (idRegion) 
+    ); 	
+    create table IF NOT EXISTS categoria (
+        idCategoria int not null auto_increment, 
+        categoria varchar(60) not  null,
+        PRIMARY KEY (idCategoria)
+    );
 
 
-CREATE TABLE IF NOT EXISTS cliente(
-	idCliente INT NOT NULL AUTO_INCREMENT , 
-    nombre varchar(60)  not NULL , 
-    correo varchar(60)  not NULL , 
-    telefono varchar(15) not null,
-    fechaRegistro DATE  null, 
-    idCiudad  INT NOT NULL , 
-    idRegion INT NOT NULL ,
-    idDireccion INT NOT NULL , 
-    primary key(idCliente), 
-    foreign key (idRegion) REFERENCES region (idRegion) 
+    CREATE TABLE IF NOT EXISTS cliente(
+        idCliente INT NOT NULL AUTO_INCREMENT , 
+        nombre varchar(60)  not NULL , 
+        correo varchar(60)  not NULL , 
+        telefono varchar(15) not null,
+        fechaRegistro DATE not null, 
+        idDireccion INT NOT NULL , 
+        primary key(idCliente), 
+        foreign key (idDireccion) REFERENCES direccion(idDireccion)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+    );
+
+    CREATE TABLE IF NOT EXISTS producto(
+        idProducto INT NOT NULL AUTO_INCREMENT, 
+        precioUnitario DECIMAL(4) NOT NULL ,
+        idCategoria int not null,
+        nombre varchar(60) not null, 
+        primary key (idProducto),
+        foreign key (idCategoria) REFERENCES categoria(idCategoria)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+        
+    );
+
+
+
+    CREATE TABLE IF NOT exists compra(
+    idCompra INT NOT NULL auto_increment, 
+    idCliente INT NOT NULL, 
+    idCompania INT NOT NULL, 
+    PRIMARY KEY (idCompra , idCliente , idCompania), 
+    foreign KEY (idCliente) references cliente(idCliente)
     ON DELETE CASCADE 
-    ON UPDATE CASCADE,
-	foreign key (idCiudad) REFERENCES ciudad(idCiudad)
-	ON DELETE CASCADE 
-    ON UPDATE CASCADE,
-	foreign key (idDireccion) REFERENCES direccion(idDireccion)
-	ON DELETE CASCADE 
+    ON UPDATE CASCADE , 
+    foreign key (idCompania) references compania(idCompania)
+    ON DELETE CASCADE 
     ON UPDATE CASCADE
-);
-CREATE TABLE IF NOT EXISTS producto(
-	idProducto INT NOT NULL AUTO_INCREMENT, 
-	precioUnitario DECIMAL(4) NOT NULL ,
-	idCategoria int not null,
-    nombre varchar(60) not null, 
-    primary key (idProducto),
-	foreign key (idCategoria) REFERENCES categoria(idCategoria)
-	ON DELETE CASCADE 
+    );
+
+
+    CREATE TABLE IF NOT EXISTS detalleCompra(
+        idDetalleCompra INT NOT NULL, 
+        idCompra INT NOT NULL,
+        idCliente INT NOT NULL,
+        idCompania INT NOT NULL,
+        idProducto INT NOT NULL,
+        cantidad INT NOT NULL, 
+        subtotal decimal(4) NOT NULL, 
+        PRIMARY KEY (idDetalleCompra),
+        FOREIGN KEY (idCompra , idCliente , idCompania) references compra(idCompra , idCliente , idCompania)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+        FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+    );
+
+
+
+    CREATE TABLE IF NOT EXISTS proveedor(
+        idProveedor INT NOT NULL AUTO_INCREMENT , 
+        nombre varchar(60) NOT NULL , 
+        correo varchar(60) NOT NULL , 
+        telefono varchar(15) not null,
+        fechaRegistro DATE not null, 
+        idDireccion INT NOT NULL , 
+        primary key(idProveedor), 
+        foreign key (idDireccion) REFERENCES direccion(idDireccion)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+    );
+
+
+    CREATE TABLE IF NOT exists orden(
+    idOrden INT NOT NULL auto_increment, 
+    idProveedor INT NOT NULL, 
+    idCompania INT NOT NULL, 
+    PRIMARY KEY (idOrden , idProveedor , idCompania), 
+    foreign KEY (idProveedor) references proveedor(idProveedor)
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE , 
+    foreign key (idCompania) references compania(idCompania)
+    ON DELETE CASCADE 
     ON UPDATE CASCADE
-    
-);
+    );
 
-
-
-CREATE TABLE IF NOT exists compra(
-idCompra INT NOT NULL auto_increment, 
-idCliente INT NOT NULL, 
-idCompania INT NOT NULL, 
-PRIMARY KEY (idCompra , idCliente , idCompania), 
-foreign KEY (idCliente) references cliente(idCliente)
-ON DELETE CASCADE 
-ON UPDATE CASCADE , 
-foreign key (idCompania) references compania(idCompania)
-ON DELETE CASCADE 
-ON UPDATE CASCADE
-);
-
-
-CREATE TABLE IF NOT EXISTS detalleCompra(
-	idDetalleCompra INT NOT NULL, 
-    idCompra INT NOT NULL,
-    idCliente INT NOT NULL,
-    idCompania INT NOT NULL,
-    idProducto INT NOT NULL,
-    cantidad INT NOT NULL, 
-    subtotal decimal(4) NOT NULL, 
-    PRIMARY KEY (idDetalleCompra),
-    FOREIGN KEY (idCompra , idCliente , idCompania) references compra(idCompra , idCliente , idCompania)
-    ON DELETE CASCADE 
-	ON UPDATE CASCADE,
-    FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
-    ON DELETE CASCADE 
-	ON UPDATE CASCADE
-);
-
-
-
-CREATE TABLE IF NOT EXISTS proveedor(
-	idProveedor INT NOT NULL AUTO_INCREMENT , 
-    nombre varchar(60) NOT NULL , 
-    correo varchar(60) NOT NULL , 
-    telefono varchar(15) not null,
-    fechaRegistro DATE not null, 
-    idCiudad  INT NOT NULL , 
-    idRegion INT NOT NULL ,
-	idDireccion INT NOT NULL , 
-    primary key(idProveedor), 
-    foreign key (idRegion) REFERENCES region (idRegion) 
-    ON DELETE CASCADE 
-    ON UPDATE CASCADE,
-    foreign key (idCiudad) REFERENCES ciudad(idCiudad)
-	ON DELETE CASCADE 
-    ON UPDATE CASCADE,
-	foreign key (idDireccion) REFERENCES direccion(idDireccion)
-	ON DELETE CASCADE 
-    ON UPDATE CASCADE
-);
-
-
-CREATE TABLE IF NOT exists orden(
-idOrden INT NOT NULL auto_increment, 
-idProveedor INT NOT NULL, 
-idCompania INT NOT NULL, 
-PRIMARY KEY (idOrden , idProveedor , idCompania), 
-foreign KEY (idProveedor) references proveedor(idProveedor)
-ON DELETE CASCADE 
-ON UPDATE CASCADE , 
-foreign key (idCompania) references compania(idCompania)
-ON DELETE CASCADE 
-ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS detalleOrden(
-	idDetalleOrden INT NOT NULL, 
-	idProducto INT NOT NULL,
-    idOrden INT NOT NULL,
-    idProveedor INT NOT NULL,
-    idCompania INT NOT NULL,
-    cantidad INT NOT NULL, 
-    subtotal decimal(4) NOT NULL, 
-    PRIMARY KEY (idDetalleOrden),
-    FOREIGN KEY (idOrden , idProveedor , idCompania) references orden(idOrden , idProveedor , idCompania)
-    ON DELETE CASCADE 
-	ON UPDATE CASCADE,
-    FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
-    ON DELETE CASCADE 
-	ON UPDATE CASCADE
-);
+    CREATE TABLE IF NOT EXISTS detalleOrden(
+        idDetalleOrden INT NOT NULL, 
+        idProducto INT NOT NULL,
+        idOrden INT NOT NULL,
+        idProveedor INT NOT NULL,
+        idCompania INT NOT NULL,
+        cantidad INT NOT NULL, 
+        subtotal decimal(4) NOT NULL, 
+        PRIMARY KEY (idDetalleOrden),
+        FOREIGN KEY (idOrden , idProveedor , idCompania) references orden(idOrden , idProveedor , idCompania)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+        FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+    );
 
 
 /*ELIMINAR TODOS LOS DATOS DE LA TABLA TEMPORAL*/

e76ae28668887da55e368153ba784b5779b49a6b
Pablo Andres Argueta Hernandez <pabloandresarg@gmail.com>
2020-10-04 01:52:38 -0600

    terminado OK
    ok falta hacerle pruebas

--- build/rutas/indexRutas.js
+++ build/rutas/indexRutas.js
@@ -15,6 +15,16 @@ class IndexRoutes {
         this.router.get('/crearTemporal', indexController_1.indexController.crearTemporal);
         this.router.get('/cargarModelo', indexController_1.indexController.crearYcargarModelo);
         this.router.get('/eliminarModelo', indexController_1.indexController.eliminarModelo);
+        this.router.get('/consulta1', indexController_1.indexController.consulta1);
+        this.router.get('/consulta2', indexController_1.indexController.consulta2);
+        this.router.get('/consulta3', indexController_1.indexController.consulta3);
+        this.router.get('/consulta4', indexController_1.indexController.consulta4);
+        this.router.get('/consulta5', indexController_1.indexController.consulta5);
+        this.router.get('/consulta6', indexController_1.indexController.consulta6);
+        this.router.get('/consulta7', indexController_1.indexController.consulta7);
+        this.router.get('/consulta8', indexController_1.indexController.consulta8);
+        this.router.get('/consulta9', indexController_1.indexController.consulta9);
+        this.router.get('/consulta10', indexController_1.indexController.consulta10);
     }
 }
 const indice = new IndexRoutes();
--- src/rutas/indexRutas.ts
+++ src/rutas/indexRutas.ts
@@ -15,7 +15,17 @@ class IndexRoutes{
    this.router.get('/eliminarTemporal',indexController.MetodoEliminarTemporal);
    this.router.get('/crearTemporal',indexController.crearTemporal);
    this.router.get('/cargarModelo',indexController.crearYcargarModelo);
-    this.router.get('/eliminarModelo' , indexController.eliminarModelo)
+   this.router.get('/eliminarModelo' , indexController.eliminarModelo);
+   this.router.get('/consulta1' , indexController.consulta1);
+   this.router.get('/consulta2' , indexController.consulta2);
+   this.router.get('/consulta3' , indexController.consulta3);
+   this.router.get('/consulta4' , indexController.consulta4);
+   this.router.get('/consulta5' , indexController.consulta5);
+   this.router.get('/consulta6' , indexController.consulta6);
+   this.router.get('/consulta7' , indexController.consulta7);
+   this.router.get('/consulta8' , indexController.consulta8);
+   this.router.get('/consulta9' , indexController.consulta9);
+   this.router.get('/consulta10' , indexController.consulta10);
   }
 
 }
